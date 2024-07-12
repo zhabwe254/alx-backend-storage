d95aceb6b164d26bd824b630c9e2ac16b55ddd3b
zhabwe254 <zhabwe@gmail.com>
2024-07-12 14:59:43 +0300

    a

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- Create SafeDiv function
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT) 
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END;//
+DELIMITER ;
